8bd5207b76d289333c1a3621ff98c5b430fa1bd7
AaronTDR <aaron.tdr12w@gmail.com>
2023-07-28 07:24:54 -0600

    Add home route - #8

--- store/src/main.tsx
+++ store/src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+// import App from "./App.tsx";
 import "./index.module.css";
+import Home from "./routes/home.tsx";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ store/src/routes/home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  const styles = {
+    color: "black",
+    fontSize: "20px",
+  };
+  return (
+    <>
+      <h2 style={styles}>FROM HOME</h2>
+    </>
+  );
+};
+
+export default Home;
