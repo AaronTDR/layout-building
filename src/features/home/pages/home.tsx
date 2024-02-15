2298529770fb892eb2f3aa7740569ac3e291e8b5
AaronTDR <aaron.tdr12w@gmail.com>
2024-02-14 21:43:28 -0600

    chore: Change the name of some elements - #27

--- src/features/home/pages/home.tsx
+++ src/features/home/pages/home.tsx
@@ -1,15 +1,15 @@
 /* components */
-import Carousel from "../../home/components/carousel/Carousel";
+import AdCarousel from "../components/carousel/AdCarousel";
 import MainHome from "../components/mainHome/MainHome";
 import Layout from "../../../components/layout/Layout";
 
 /* styles */
-import container from "../stylesHome/container.module.css";
+import styles from "../stylesHome/container.module.css";
 
 const Home = () => {
   return (
-    <Layout css={container.container}>
-      <Carousel />
+    <Layout css={styles.container}>
+      <AdCarousel />
       <MainHome />
     </Layout>
   );
