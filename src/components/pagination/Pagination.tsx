5b3163965e72460a9ee43475f27aa217cd1e1643
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-04-15 15:05:36 -0600

    feat: Disabling Paging Buttons When There Are No Additional Pages - #30
    Disables the 'previous' and 'next' paging buttons if there is no page accessible from the current position.

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,14 +1,4 @@
-import { useContext } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
-
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-
-/* Components */
-import Icon from "../../components/icon/Icon";
 import RenderPageNumbers from "./renderPageNumbers/RenderPageNumbers";
-
-/* Theme context */
-import { ThemeContext } from "../../contexts/ThemeProvider";
 
 /* Styles */
 import styles from "./pagination.module.css";
@@ -21,41 +11,15 @@ const Pagination = ({
   itemsPerPage,
   maxPagesToShow,
 }: PaginationType) => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const query = searchParams.get("q") || "";
-  const { page } = useParams();
-  const currentPage: number = Number(page);
-
-  const { isDarkMode } = useContext(ThemeContext);
-
-  const buttonTheme = isDarkMode
-    ? styles.buttonsDarkMode
-    : styles.buttonsLightMode;
-
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   return (
     <div className={styles.pagination}>
       <ul className={styles.ul}>
-        <Link
-          to={`/search/${currentPage - 1}?q=${encodeURIComponent(query)}`}
-          key="previous"
-          className={`${styles.buttons} ${buttonTheme}`}
-        >
-          <Icon icon={faArrowLeft} css="" />
-        </Link>
         <RenderPageNumbers
           totalPages={totalPages}
           maxPagesToShow={maxPagesToShow}
         />
-        <Link
-          to={`/search/${currentPage + 1}?q=${encodeURIComponent(query)}`}
-          key="next"
-          className={`${styles.buttons} ${buttonTheme}`}
-        >
-          <Icon icon={faArrowRight} css="" />
-        </Link>
       </ul>
     </div>
   );
--- src/components/pagination/renderPageNumbers/RenderPageNumbers.tsx
+++ src/components/pagination/renderPageNumbers/RenderPageNumbers.tsx
@@ -1,5 +1,10 @@
 import { useContext } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+/* Components */
+import Button from "../../button/Button";
 
 /* Theme context */
 import { ThemeContext } from "../../../contexts/ThemeProvider";
@@ -16,11 +21,18 @@ const RenderPageNumbers = ({
 }: RenderPageNumberType) => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
+
   const query = searchParams.get("q") || "";
   const { page } = useParams();
   const currentPage: number = Number(page);
 
+  const navigate = useNavigate();
+
   const { isDarkMode } = useContext(ThemeContext);
+
+  const buttonTheme = isDarkMode
+    ? styles.buttonsDarkMode
+    : styles.buttonsLightMode;
 
   const pageNumberTheme = isDarkMode
     ? styles.pageNumberDarkMode
@@ -40,6 +52,36 @@ const RenderPageNumbers = ({
   if (endPage - startPage < maxPagesToShow - 1) {
     startPage = Math.max(2, endPage - maxPagesToShow + 1);
   }
+
+  /* Determines whether the button is enabled or not */
+  const isDisabledPrev = currentPage === 1;
+  const isDisabledNext = currentPage === totalPages;
+
+  /* If the button is disabled, remove the cursor pointer */
+  const disabledPrevStyle = {
+    opacity: isDisabledPrev ? 0.5 : 1,
+    cursor: isDisabledPrev ? "not-allowed" : "pointer",
+  };
+
+  const disabledNextStyle = {
+    opacity: isDisabledNext ? 0.5 : 1,
+    cursor: isDisabledNext ? "not-allowed" : "pointer",
+  };
+
+  /* Manage page changes by using the previous and next buttons */
+  const handlePrev = () => {
+    if (isDisabledPrev) {
+      return;
+    }
+    navigate(`/search/${currentPage - 1}?q=${encodeURIComponent(query)}`);
+  };
+
+  const handleNext = () => {
+    if (isDisabledNext) {
+      return;
+    }
+    navigate(`/search/${currentPage + 1}?q=${encodeURIComponent(query)}`);
+  };
 
   // Always show page 1
   pageNumbers.push(
@@ -107,7 +149,29 @@ const RenderPageNumbers = ({
   );
 
   // Return page numbers
-  return <>{pageNumbers}</>;
+  return (
+    <>
+      <Button
+        key={"previous"}
+        onClick={handlePrev}
+        disabled={isDisabledPrev}
+        cssButton={`${styles.buttons} ${buttonTheme}`}
+        icon={faArrowLeft}
+        cssIcon=""
+        inlineStyles={disabledPrevStyle}
+      />
+      {pageNumbers}
+      <Button
+        key={"next"}
+        onClick={handleNext}
+        disabled={isDisabledNext}
+        cssButton={`${styles.buttons} ${buttonTheme}`}
+        icon={faArrowRight}
+        cssIcon=""
+        inlineStyles={disabledNextStyle}
+      />
+    </>
+  );
 };
 
 export default RenderPageNumbers;
