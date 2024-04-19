bc236b3473a557f8725fcb3341a59fa7c78ed187
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-04-18 20:30:42 -0600

    feat: Add error message for domain retrieval failure - #31

--- src/features/domain/pages/Domain.tsx
+++ src/features/domain/pages/Domain.tsx
@@ -1,4 +1,14 @@
 import { useParams } from "react-router-dom";
+
+import {
+  faFaceSadTear,
+  faMagnifyingGlassMinus,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* Components */
+import Message from "../../../components/message/Message";
+
+/* Utils */
 import { getDomains } from "./utils/getDomains";
 
 const Domain = () => {
@@ -6,13 +16,28 @@ const Domain = () => {
 
   const domains = getDomains();
 
-  if (!name || !domains) return <div>Domain not found</div>;
+  if (!name || !domains) {
+    // return <div>Domain not found</div>;
+    return (
+      <Message
+        icon={faFaceSadTear}
+        title="Something went wrong"
+        message="Sorry, we were unable to complete your action. Please try again later."
+      />
+    );
+  }
 
   const domain = domains.find((d) => d.toLowerCase() === name.toLowerCase());
 
+  // Handle the case where the domain is not found
   if (!domain) {
-    // Manejar el caso en que el dominio no se encuentra
-    return <div>Domain not found</div>;
+    return (
+      <Message
+        icon={faMagnifyingGlassMinus}
+        title="Domain not found"
+        message="Oops! It seems that the domain you’re trying to access is not available."
+      />
+    );
   }
 
   return <div>Domain: {domain}</div>;
