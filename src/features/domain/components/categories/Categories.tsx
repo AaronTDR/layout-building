1158e8355515dc8b1be58a3c1f0b6ad8173a05f3
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-04-22 21:40:02 -0600

    styles: Add theme to categories - #31

--- src/features/domain/components/categories/Categories.tsx
+++ src/features/domain/components/categories/Categories.tsx
@@ -1,6 +1,10 @@
 /* Components */
+import { useContext } from "react";
 import Icon from "../../../../components/icon/Icon";
 import TextTruncate from "../../../../components/textTruncate/TextTruncate";
+
+/* Context theme */
+import { ThemeContext } from "../../../../contexts/ThemeProvider";
 
 /* Styles */
 import styles from "./categories.module.css";
@@ -9,19 +13,34 @@ import styles from "./categories.module.css";
 import { CategoriesType } from "./CategoriesType";
 
 const Categories = ({ domain }: CategoriesType) => {
+  // Get the theme context
+  const themeContext = useContext(ThemeContext);
+  const { isDarkMode } = themeContext;
+
+  const iconTheme = isDarkMode ? styles.iconDarkMode : styles.iconLightMode;
+
+  const nameTheme = isDarkMode ? styles.nameDark : styles.nameLight;
+
   return (
     <ul className={styles.categoriesList}>
       {domain.categories.map((category) => (
         <li key={category.id} className={styles.category}>
           <span className={styles.iconContainer}>
-            <Icon icon={category.icon} css={styles.icon} />
+            <Icon
+              icon={category.icon}
+              css={`
+                ${iconTheme} ${styles.icon}
+              `}
+            />
           </span>
           <div className={styles.nameContainer}>
             <TextTruncate
               text={category.name}
               rows={3}
               tagType={"h2"}
-              css={styles.name}
+              css={`
+                ${nameTheme} ${styles.name}
+              `}
             />
           </div>
         </li>
