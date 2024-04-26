48906a541e45e5450eab7018cba1d554aba9fb69
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-04-25 20:44:00 -0600

    feat: Apply logic to make infinite scroll work - #31

--- src/features/domain/pages/Domain.tsx
+++ src/features/domain/pages/Domain.tsx
@@ -21,6 +21,7 @@ import styles from "./domain.module.css";
 import { getDomains } from "../utils/getDomains";
 
 const Domain = () => {
+  const [isFirstRender, setIsFirstRender] = useState(true);
   const [isAtBottom, setIsAtBottom] = useState(false);
 
   const [currentCategory, setCurrentCategory] = useState(0);
@@ -45,6 +46,7 @@ const Domain = () => {
       const scrolledToBottom =
         window.innerHeight + document.documentElement.scrollTop ===
         document.documentElement.offsetHeight;
+      console.log("🚀 ~ onScroll ~ scrolledToBottom:", scrolledToBottom);
       setIsAtBottom(scrolledToBottom);
     };
 
@@ -53,41 +55,40 @@ const Domain = () => {
   }, []);
 
   useEffect(() => {
-    if (!domain) {
-      setError("Domain not found");
-      return;
-    }
+    if (domain) {
+      if (isFirstRender) {
+        setIsFirstRender(false);
+      } else {
+        if (name !== nameState) {
+          console.log("name changed!", name);
 
-    if (name !== nameState) {
-      console.log("I reset values");
-      setResults([]);
-      setCurrentCategory(0);
-      setOffset(0);
-      console.log("I made the request!");
-      setNameState(name);
-      fetchResults(domain.categories[currentCategory]?.id, offset);
-    }
-  }, [name]);
-
-  useEffect(() => {
-    if (isAtBottom) {
-      // Aquí puedes realizar tu petición a la API
-      console.log("MAKES FIRST REQUEST");
-      if (domain) {
-        if (currentCategory + 1 === domain.categories.length) {
-          console.log("INTO CURRENT === LENGTH");
+          setNameState(name);
+          setResults([]);
           setCurrentCategory(0);
-          setOffset((prevSetOffset) => prevSetOffset + 20);
-        } else {
-          setCurrentCategory((prevCurrent) => prevCurrent + 1);
-        }
-        console.log("🚀 ~ useEffect ~ currentCategory:", currentCategory);
-        console.log("🚀 ~ Domain ~ offset:", offset);
+          setOffset(0);
 
-        fetchResults(domain.categories[currentCategory]?.id, offset);
+          // Llama a fetchResults aquí
+          fetchResults(domain.categories[0]?.id, 0);
+        } else if (currentCategory + 1 === domain.categories.length) {
+          setOffset((prevOffset) => prevOffset + limit);
+          setCurrentCategory(0);
+        } else {
+          console.log(
+            "Fourth effect at condition-> domain exists and name === nameState)"
+          );
+
+          setCurrentCategory((currentCategory) => currentCategory + 1);
+        }
       }
     }
-  }, [isAtBottom]);
+  }, [isAtBottom, name]);
+
+  useEffect(() => {
+    console.log("new request");
+    if (domain) {
+      fetchResults(domain.categories[currentCategory]?.id, offset);
+    }
+  }, [currentCategory, offset]);
 
   const fetchResults = async (categoryId: string, offset: number) => {
     console.log("🚀 ~ fetchResults ~ offset:", offset);
@@ -110,8 +111,6 @@ const Domain = () => {
         );
         return [...prevResults, ...newResults];
       });
-
-      return items; // ! Quit after tests
     } catch (error) {
       console.error("Error fetching results:", error);
       setError("Error fetching results");
@@ -144,6 +143,21 @@ const Domain = () => {
         <Categories domain={domain} />
       </main>
       {/* <MainResults results={results} /> */}
+      {console.log(
+        "Category: ",
+        domain?.categories[currentCategory]?.name,
+        "\n\n",
+        "ID: ",
+        domain?.categories[currentCategory]?.id,
+        "\n\n",
+        "offset: ",
+        offset,
+        "\n\n",
+        "current: ",
+        currentCategory,
+        "\n\n",
+        "Results: "
+      )}
       {/* {results} */ console.log(results)}
     </Layout>
   );
