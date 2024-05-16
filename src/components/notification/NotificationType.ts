8f95adc4a343d2c519fa9a51251dc446586cf867
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-05-15 19:56:24 -0600

    type: Add correct typing to Notification - #39

--- src/components/notification/NotificationType.ts
+++ src/components/notification/NotificationType.ts
@@ -1,13 +1,26 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-export type NotificationType = {
+type BaseNotificationType = {
   slipDirection: "toRightTop" | "toRightBottom" | "toLeftTop" | "toLeftBottom";
   type: "success" | "warning" | "notice" | "fail";
   css?: string;
   message: string;
   icon?: IconDefinition;
   displayDuration?: number;
-  actionButton?: boolean;
-  textButton?: string;
-  onPressButton?: () => void;
 };
+
+type NotificationWithButton = BaseNotificationType & {
+  actionButton: true;
+  textButton: string;
+  onPressButton: () => void;
+};
+
+type NotificationWithoutButton = BaseNotificationType & {
+  actionButton?: false;
+  textButton?: never;
+  onPressButton?: never;
+};
+
+export type NotificationType =
+  | NotificationWithButton
+  | NotificationWithoutButton;
