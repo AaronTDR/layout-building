ab1ce60cdef349fa17c3dbbea8f7dbef1caa95a0
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-06-04 12:37:40 -0600

    feat: Add buildUrl.ts function - #41

---
+++ src/utils/buildUrl/buildUrl.ts
@@ -0,0 +1,14 @@
+export const buildUrl = (type, endpoint, params = {}) => {
+  const queryParams = Object.keys(params)
+    .map((key) => {
+      if (type === "SEARCH") {
+        return `${key}=${encodeURIComponent(params[key])}`;
+      } else if (type === "MULTIGET") {
+        return `${key}=${params[key]}`;
+      }
+      return "";
+    })
+    .join("&");
+
+  return `${BASE_URL}${endpoint}?${queryParams}`;
+};
